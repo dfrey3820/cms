204d9a0034cad419541c5173ecd6a152ff95513e
dfrey3820 <168394379+dfrey3820@users.noreply.github.com>
2026-01-20 13:19:48 +0300

    Add auto-migration for tables if they don't exist

--- src/Providers/CmsServiceProvider.php
+++ src/Providers/CmsServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Buni\Cms\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
 use Buni\Cms\Services\PluginManager;
 use Buni\Cms\Services\ThemeManager;
 use Buni\Cms\Services\HookManager;
@@ -25,6 +27,11 @@ class CmsServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../../routes/cms.php');
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'cms');
+
+        // Auto-run migrations if tables don't exist
+        if (!Schema::hasTable('pages')) {
+            Artisan::call('migrate', ['--force' => true]);
+        }
 
         if ($this->app->runningInConsole()) {
             $this->commands([
